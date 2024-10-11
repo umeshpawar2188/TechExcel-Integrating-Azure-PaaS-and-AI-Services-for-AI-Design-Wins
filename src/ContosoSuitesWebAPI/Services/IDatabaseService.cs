7482da098454a2ba9b6317c8319773a92e9e47d4
Umesh Pawar <umeshpawar@microsoft.com>
2024-10-11 12:15:04 +0100

    commit 2.2

--- src/ContosoSuitesWebAPI/Services/IDatabaseService.cs
+++ src/ContosoSuitesWebAPI/Services/IDatabaseService.cs
@@ -4,9 +4,9 @@ namespace ContosoSuitesWebAPI.Services;
 
 public interface IDatabaseService
 {
-    //Task<IEnumerable<Hotel>> GetHotels();
-    //Task<IEnumerable<Booking>> GetBookingsForHotel(int hotelId);
-    //Task<IEnumerable<Booking>> GetBookingsByHotelAndMinimumDate(int hotelId, DateTime dt);
+    Task<IEnumerable<Hotel>> GetHotels();
+    Task<IEnumerable<Booking>> GetBookingsForHotel(int hotelId);
+    Task<IEnumerable<Booking>> GetBookingsByHotelAndMinimumDate(int hotelId, DateTime dt);
 
     Task<IEnumerable<Booking>> GetBookingsMissingHotelRooms();
     Task<IEnumerable<Booking>> GetBookingsWithMultipleHotelRooms();
